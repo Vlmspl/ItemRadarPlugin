7204805ad40344c0db49316108b093680ff137e0
bilou <bilousvlad2020@gmail.com>
2024-01-31 17:04:56 +0200

    Add project files.

---
+++ ItemRadarPlugin/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+
+namespace ItemRadarPlugin
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "poseidon.itemradar";
+        private const string modName = "Item Radar Plugin";
+        private const string modVersion = "1.0.0";
+
+        private static Plugin instance;
+        internal ManualLogSource logger;
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            logger.LogInfo("Initializing ItemRadar Plugin...");
+
+            // Additional initialization code if needed
+
+            RegisterPatches();
+            RegisterNetworking();
+            RegisterItems();
+
+            logger.LogInfo("ItemRadarPlugin:1.0.0 has successfully loaded!");
+        }
+
+        private void RegisterPatches()
+        {
+            logger.LogInfo("Applying patches.");
+            
+            // Add other patches as needed
+        }
+
+        private void RegisterNetworking()
+        {
+            logger.LogInfo("Doing networky stuff");
+            
+        }
+
+        private void RegisterItems()
+        {
+            logger.LogInfo("Registering Items");
+            
+        }
+    }
+}
---
+++ ItemRadarPlugin/PluginBase/Assets.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace ItemRadarPlugin.PluginBase
+{
+    internal class Assets
+    {
+        internal static AssetBundle? assetBundle;
+        internal static GameObject? radarPrefab;        // Assuming you have a prefab for the radar
+        internal static Texture2D? radarTexture1;      // First texture for the radar
+        internal static Texture2D? radarTexture2;      // Second texture for the radar
+
+        public static void Init()
+        {
+            // Modify the path to load the asset bundle from the "PluginBundles" folder
+            string bundlePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "radar");
+
+            assetBundle = AssetBundle.LoadFromFile(bundlePath);
+
+            // Load the radar prefab and textures from the asset bundle
+            radarPrefab = assetBundle?.LoadAsset<GameObject>("Radar.fbx");
+            radarTexture1 = assetBundle?.LoadAsset<Texture2D>("Untitled.001.png");
+            radarTexture2 = assetBundle?.LoadAsset<Texture2D>("EchoLocator.png");
+
+            // Add more asset loading as needed
+        }
+    }
+}
